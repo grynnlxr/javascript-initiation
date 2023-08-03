0853bd4392a30639cd3e75cfe44ec573e6315203
Chris Lisangola <jochri3@gmail.com>
2023-08-03 11:22:23 +0300

    2.Variables avec

---
+++ app.java
@@ -0,0 +1,17 @@
+System.out.print(name);
+String name="Dad";
+
+
+
+for(int i=0;i<3;i++){
+    System.out.print(i)//0 1 2
+}
+
+System.out.print(i)//3
+
+if(age>=18){
+    int temp=age*4;
+
+}
+
+System.out.print(age)
